5f25af17b8a83b0b910e774829551c90db438c49
Shiva Kumar H R <shivahr@gmail.com>
2022-01-08 08:07:31 +0530

    Update git ignore

--- scripts/train_te_m_rp.sh
+++ scripts/train_te_m_rp.sh
@@ -7,3 +7,4 @@ INPUT_VALIDATION_FILE=./${DATASET_NAME}/filelists/audio_text_val.txt
 OUTPUT_DIR=./output/${DATASET_NAME}/
 
 python train.py --input_wavs_dir ${INPUT_WAVS_DIR} --input_mels_dir ${INPUT_MELS_DIR} --input_training_file ${INPUT_TRAINING_FILE} --input_validation_file ${INPUT_VALIDATION_FILE} --checkpoint_path ${OUTPUT_DIR} --fine_tuning True --config config_v1.json
+
