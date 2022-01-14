75da4de0aa26ee5ec1465d2d4a62fcf9c1fe41f4
Shiva Kumar H R <shivahr@gmail.com>
2022-01-14 10:48:01 +0530

    Remove fine_tuning for now

--- scripts/train_te_m_rp.sh
+++ scripts/train_te_m_rp.sh
@@ -6,4 +6,4 @@ INPUT_TRAINING_FILE=./datasets/${DATASET_NAME}/filelists/audio_text_train.txt
 INPUT_VALIDATION_FILE=./datasets/${DATASET_NAME}/filelists/audio_text_val.txt
 OUTPUT_DIR=./output/${DATASET_NAME}/
 
-python train.py --input_wavs_dir ${INPUT_WAVS_DIR} --input_mels_dir ${INPUT_MELS_DIR} --input_training_file ${INPUT_TRAINING_FILE} --input_validation_file ${INPUT_VALIDATION_FILE} --checkpoint_path ${OUTPUT_DIR} --fine_tuning True --config config_v1.json
+python train.py --input_wavs_dir ${INPUT_WAVS_DIR} --input_mels_dir ${INPUT_MELS_DIR} --input_training_file ${INPUT_TRAINING_FILE} --input_validation_file ${INPUT_VALIDATION_FILE} --checkpoint_path ${OUTPUT_DIR} --config config_v1.json
