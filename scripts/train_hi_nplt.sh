1be503521e138d55fce21f473c73bebf4762f066
Shiva Kumar H R <shivahr@gmail.com>
2022-01-26 14:08:36 +0530

    Script for multi-speaker training on hi_nplt dataset

---
+++ scripts/train_hi_nplt.sh
@@ -0,0 +1,9 @@
+DATASET_NAME=hi_nplt
+
+INPUT_WAVS_DIR=./datasets/${DATASET_NAME}/wavs
+INPUT_MELS_DIR=./datasets/${DATASET_NAME}/mels
+INPUT_TRAINING_FILE=./datasets/${DATASET_NAME}/filelists/hifigan_audio_text_train.txt
+INPUT_VALIDATION_FILE=./datasets/${DATASET_NAME}/filelists/hifigan_audio_text_val.txt
+OUTPUT_DIR=./output/${DATASET_NAME}/
+
+python train.py --input_wavs_dir ${INPUT_WAVS_DIR} --input_mels_dir ${INPUT_MELS_DIR} --input_training_file ${INPUT_TRAINING_FILE} --input_validation_file ${INPUT_VALIDATION_FILE} --checkpoint_path ${OUTPUT_DIR} --config config_v1.json
